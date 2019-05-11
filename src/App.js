c0fff10bcae12d67874686e855b0ec295d69bc55
Kingsley Silas <kinsomicrote.inc@gmail.com>
2019-05-11 08:54:11 +0100

    rewrote App component

--- src/App.js
+++ src/App.js
@@ -1,26 +1,63 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import { createContainer } from "unstated-next";
+import shortid from "shortid";
 
-function App() {
+const useTodo = () => {
+  const list = [
+    { id: 1, title: 'Write code' },
+    { id: 2, title: 'Buy milk' }
+  ]
+  const [todos, addTodo] = useState(list);
+  const [item, setTodo] = useState("");
+
+  const handleTodo = event => {
+    setTodo(event.target.value);
+  };
+
+  const handleSubmit = event => {
+    event.preventDefault();
+    const value = {
+      id: shortid.generate(),
+      title: item
+    }
+    addTodo(todos.concat(value));
+    setTodo("");
+  };
+
+  return {
+    todos,
+    item,
+    handleTodo,
+    handleSubmit
+  };
+};
+
+const Todo = createContainer(useTodo);
+
+const TodoContainer = () => {
+  const todo = Todo.useContainer();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <p>Add Todos</p>
+      <input type="text" value={todo.item} onChange={todo.handleTodo} />
+      <button onClick={todo.handleSubmit}>Add</button>
+      <div>
+        {todo.todos.map((item) => {
+          return (
+            <ul key={item.id}>
+              <li>{item.title}</li>
+            </ul>
+          );
+        })}
+      </div>
     </div>
   );
-}
+};
+
+const App = () => (
+  <Todo.Provider>
+    <TodoContainer />
+  </Todo.Provider>
+)
 
 export default App;
